ec719d65447d0db3344e788aa18ba72e32618ac9
Tyson <381980920@qq.com>
2019-06-04 17:14:49 +0800

    update

--- CSVprocessor.py
+++ CSVprocessor.py
@@ -4,6 +4,7 @@ day = pd.read_csv('map_min\day3.csv')
 
 for i in range(len(day.x)):
     day.x[i] += 1
+    day.y[i] += 1
     if day.nop[i] != 0:
         day.nop[i] = (day.nop[i] + 59)// 60
         # print(day.nop[i])
--- huichangrenshu.py
+++ huichangrenshu.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import copy
+import pickle
 
-day = pd.read_csv('map_min\day01.csv')
+day = pd.read_csv('map_min\day03.csv')
 day = day.sort_values(by=['time','floor'], ascending=True)
 print(day)
 
@@ -42,38 +44,58 @@ second_floor = [
     "#EEEEEE....ff##################",
     "#EEEEEE########################"
 ]
-A = {}; B = {}; C = {}; D = {}; Q = {}; P = {}; T1 = {}; T2 = {}; 
-R1 = {}; R2 = {}; R3 = {}; R4 = {}; S = {}; M = {}; H = {}; 
-R = {}; R5 = {}; R6 = {}; E = {}; T3 = {}; 
+# A = {}; B = {}; C = {}; D = {}; Q = {}; P = {}; T1 = {}; T2 = {}; 
+# R1 = {}; R2 = {}; R3 = {}; R4 = {}; S = {}; M = {}; H = {}; 
+# R = {}; R5 = {}; R6 = {}; E = {}; T3 = {}; 
+
+data = {'first_floor':{}, 'second_floor':{}}
+
 for i in range(len(day.nop)):
-    if day.floor[i] == 1 and first_floor[day.x[i]][day.y[i]] == 'A':
-        if day.time[i] in A:
-            A[day.time[i]] += day.nop[i]
-        else:
-            A[day.time[i]] = day.nop[i]
-    elif day.floor[i] == 1 and first_floor[day.x[i]][day.y[i]] == 'B':
-        if day.time[i] in B:
-            B[day.time[i]] += day.nop[i]
-        else:
-            B[day.time[i]] = day.nop[i]
-    elif day.floor[i] == 1 and first_floor[day.x[i]][day.y[i]] == 'C':
-        if day.time[i] in C:
-            C[day.time[i]] += day.nop[i]
-        else:
-            C[day.time[i]] = day.nop[i]
-    elif day.floor[i] == 1 and first_floor[day.x[i]][day.y[i]] == 'D':
-        if day.time[i] in D:
-            D[day.time[i]] += day.nop[i]
-        else:
-            D[day.time[i]] = day.nop[i]
-    elif day.floor[i] == 1 and first_floor[day.x[i]][day.y[i]] == 'Q':
-        if day.time[i] in Q:
-            Q[day.time[i]] += day.nop[i]
-        else:
-            Q[day.time[i]] = day.nop[i]
-    elif day.floor[i] == 1 and first_floor[day.x[i]][day.y[i]] == 'P':
-        if day.time[i] in P:
-            P[day.time[i]] += day.nop[i]
-        else:
-            P[day.time[i]] = day.nop[i]
+    if day.floor[i] == 1:
+        floor = data['first_floor']
+        label = first_floor[day.x[i]][day.y[i]]
+    else:
+        floor = data['second_floor']
+        label = second_floor[day.x[i]][day.y[i]]
+    if label not in floor:
+        floor[label] = {}
+    floor[label][day.time[i]] = day.nop[i] + floor[label].get(day.time[i],0)
+
+data_output = open('map_min/day3.pkl','wb')
+pickle.dump(data, data_output)
+data_output.close()
+
 print(0)
+
+# for i in range(len(day.nop)):
+#     if day.floor[i] == 1 and first_floor[day.x[i]][day.y[i]] == 'A':
+#         if day.time[i] in A:
+#             A[day.time[i]] += day.nop[i]
+#         else:
+#             A[day.time[i]] = day.nop[i]
+#     elif day.floor[i] == 1 and first_floor[day.x[i]][day.y[i]] == 'B':
+#         if day.time[i] in B:
+#             B[day.time[i]] += day.nop[i]
+#         else:
+#             B[day.time[i]] = day.nop[i]
+#     elif day.floor[i] == 1 and first_floor[day.x[i]][day.y[i]] == 'C':
+#         if day.time[i] in C:
+#             C[day.time[i]] += day.nop[i]
+#         else:
+#             C[day.time[i]] = day.nop[i]
+#     elif day.floor[i] == 1 and first_floor[day.x[i]][day.y[i]] == 'D':
+#         if day.time[i] in D:
+#             D[day.time[i]] += day.nop[i]
+#         else:
+#             D[day.time[i]] = day.nop[i]
+#     elif day.floor[i] == 1 and first_floor[day.x[i]][day.y[i]] == 'Q':
+#         if day.time[i] in Q:
+#             Q[day.time[i]] += day.nop[i]
+#         else:
+#             Q[day.time[i]] = day.nop[i]
+#     elif day.floor[i] == 1 and first_floor[day.x[i]][day.y[i]] == 'P':
+#         if day.time[i] in P:
+#             P[day.time[i]] += day.nop[i]
+#         else:
+#             P[day.time[i]] = day.nop[i]
+    
